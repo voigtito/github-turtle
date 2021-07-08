e43bdbb61d24ca471dd7ab0836b93fa2c4093e0e
voigtito <voigt.gustavoh@gmail.com>
2021-07-08 16:23:21 -0300

    Finished github api connect with axios

--- src/routes/github.routes.ts
+++ src/routes/github.routes.ts
@@ -1,9 +1,56 @@
 import { Router, Request, Response } from 'express';
+import axios from 'axios';
 
 const githubRouter = Router();
 
-githubRouter.get('/repositories', async (request:Request, response:Response) => {
-  
+interface Commit {
+  commit: {
+    author: {
+      name: string;
+      email: string;
+      date: string | Date;
+    },
+    message: string;
+  }
+  author: {
+    login: string;
+    id: number;
+  },
+}
+
+interface ContributorsCommits {
+  commits: Commit[]
+}
+
+/**
+ * *This function returns all the contributors and its commits from the repo
+ */
+githubRouter.get('/repository', async (request: Request, response: Response) => {
+
+  const { user, repository, since, until } = request.query;
+
+  let formattedDateSince = new Date(String(since)).toISOString();
+  let formattedDateUntil = new Date(String(until)).toISOString();
+  let repositoryData
+
+  try {
+    repositoryData = await axios.get<ContributorsCommits>(`https://api.github.com/repos/${user}/${repository}/commits`,
+      {
+        headers: {
+          'Authorization': `Bearer ${process.env.GITHUB_KEY}`
+        },
+        params: {
+          since: formattedDateSince,
+          until: formattedDateUntil
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error)
+  }
+
+  return response.json(repositoryData?.data);
+
 });
 
 export default githubRouter;
--- src/service/apiGithub.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: "https://api.github.com/",
-  timeout: 10,
-  headers: {
-    Authorization: `Bearer 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`
-  }
-});
